648528be282f4063e804baebcf168db28be4cc11
Tanhoccode <tanprovip156@gmail.com>
2025-01-08 01:33:48 +0700

    Login Sigin

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+void main() async {
+  
+    child: const MyApp(),
+
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<AuthBloc>().add(AuthIsUserLoggedIn());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Blog App',
+      theme: AppTheme.darkThemeMode,
+      home: BlocSelector<AppUserCubit, AppUserState, bool>(
+        selector: (state) {
+          return state is AppUserLoggedIn;
+        },
+        builder: (context, isLoggedIn) {
+          if (isLoggedIn) {
+            return const BlogPage();
+          }
+          return const LoginPage();
+        },
+      ),
+    );
+  }
+}
